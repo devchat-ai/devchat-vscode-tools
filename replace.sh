b9f03afb9dc4a12dc41d6f2ba91b7d42874bc11a
bobo.yang <bobo.yang@merico.dev>
2023-11-03 15:10:56 +0800

    To adapt nexworkx for Python 3.8 version

--- replace.sh
+++ replace.sh
@@ -3,5 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ABSOLUTE_PATH2="$DIR/site-packages/openai/_types.py"
+NETWORKX_ATLAS="$DIR/site-packages/networkx/generators/atlas.py"
 
 sed -i '' 's|FileContent = Union\[IO\[bytes\], bytes, PathLike\[str\]\]|FileContent = Union\[IO\[bytes\], bytes, PathLike\]|' "$ABSOLUTE_PATH2"	
+
+sed -i '' 's|importlib\.resources|importlib_resources|' "$NETWORKX_ATLAS"	
+
